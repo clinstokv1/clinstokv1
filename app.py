e882fa1d494d11fb621dcda875dd69bd51f2f17f
clinstokv1 <clinstok@gmail.com>
2025-07-21 17:25:44 -0400

    Deploy: estrutura inicial com HTML, CSS e JS

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Clinstok - Sistema de Estoque</h1><p>Página inicial em construção.</p>"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,1 @@
+console.log("Clinstok carregado com sucesso.");
